f33e3a98290a936bd1904e34545949617451fc50
STG996 <sami.zayed8@gmail.com>
2025-07-13 14:54:53 +0300

    Fix cursor bug

--- client.py
+++ client.py
@@ -1,23 +1,27 @@
 import socket
 import threading
 import sys
+import os
+
+CLEAR = "clear" if os.name != "nt" else "cls"
 
 HOST = socket.gethostname()
 PORT = 5000
 MAX_MSG_LEN = 2048
 
+cursor_pos = 6
+
 def recv_msgs(server: socket.socket):
+    global cursor_pos
     while True:
         message = server.recv(MAX_MSG_LEN).decode()
-        sys.stdout.write("\x1b[u")
+        sys.stdout.write(f"\033[{cursor_pos};0H")  # Moves cursor back to last position
         print(message)
-        sys.stdout.write("\x1b[s")
-
+        cursor_pos += 1
 
 def send_msgs(server: socket.socket):
     while True:
-        sys.stdout.write("\x1b[6;3H\x1b[K")
-        message = sys.stdin.readline()
+        message = input()
         server.send(message.encode())
 
 def main():
@@ -30,14 +34,11 @@ def main():
 
     server.send(username.encode())
 
-    sys.stdout.write("\x1b[2J\x1b[H")
+    os.system(CLEAR)
     print("***********************")
     print("* Real Time Messenger *")
     print("*   (C) 2025 STG996   *")
     print("***********************\n")
-    print("> ")
-    print("\n\n")
-    sys.stdout.write("\x1b[s")
 
     threading.Thread(target=send_msgs, args={server,}).start()
     threading.Thread(target=recv_msgs, args={server,}).start()
