d7ef1e87536b254ede68845100eb5502544adb8f
STG996 <sami.zayed8@gmail.com>
2025-07-15 17:16:48 +0300

    Add client-side end-to-end encryption

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import socket
 import threading
 import sys
 import os
+from random import randint
 
 CLEAR = "clear" if os.name != "nt" else "cls"
 
@@ -9,6 +10,23 @@ HOST = socket.gethostname()
 PORT = 5000
 MAX_MSG_LEN = 2048
 
+class Client:
+    def __init__(self):
+        self.__private_key = randint(1000000, 9999999)
+        self.__shared_key = None
+
+    def contribute(self, g, n):
+        result = (g ** self.__private_key) % n
+        return result
+
+    def set_shared_key(self, g, n):
+        self.__shared_key = self.contribute(g, n)
+
+    def encrypt_msg(self, msg):
+        pass
+
+    def decrypt_msg(self, msg):
+        pass
 
 def recv_msgs(server: socket.socket):
     cursor_pos = 6
@@ -31,7 +49,21 @@ def main():
     while username == "" or len(username) > MAX_MSG_LEN:
         username = input("Enter username: ")
 
+    # Initialise client object and send username
+    client = Client()
     server.send(username.encode())
+
+    # Diffie-Hellman key exchange
+    n = int(server.recv(MAX_MSG_LEN).decode())
+    final = False
+    while not final:
+        recvd = server.recv(MAX_MSG_LEN).decode()
+        if recvd == "UPCOMING FINAL":
+            final = True
+        else:
+            server.send(client.contribute(int(recvd), n))
+    recvd = int(server.recv(MAX_MSG_LEN).decode())
+    client.set_shared_key(recvd, n)
 
     os.system(CLEAR)
     print("***********************")
